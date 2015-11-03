83b905b5e538ccf84dd181888b03a75d0cabf06d
suvradip <suvradip@fusioncharts.com>
2015-11-03 17:00:02 +0530

    changes

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+#This is a shell script which help us to run several build command for Dev center in one place.
+
+# How to run this script ?
+# To initialize the build , try to run this command from terminal within paradocs folder
+
+# sh ./build.sh _command_
+# for example sh ./build.sh ALL
+
+# List of available commands
+
+# 1. ALL-  complete dev center build Process.
+# 2. DEV -  build for FusioncCharts
+# 3. LOCAL - build for Local System
+# 4. API-DATA - generate api-data.json
+# 5. TOC - to generate table of content base on baseurl in _config.yml file.
+# 6. CFP - to change file permission of out folder to 777
+
+
 allBuild ()
 {
 	apiDataJson
